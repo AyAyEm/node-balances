0461bb663087d03239ca38204cb0c3e2f0c07cc1
WarNote <andre_20113meira@hotmail.com>
2021-01-13 10:47:30 -0300

    Fix: removed unecessary file

--- src/examples/newTest.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs/promises';
-
-fs.readFile('./samples/uranoPop/use-p2').then((data) => console.log(data));
-
-console.log(Buffer.from('T2BN0           0,080 kg      0,00 N1     0,00 E'));
-
-export default 'teste';
