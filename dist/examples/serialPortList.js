ed81ccfebe3316469880a8b652e7651c2a9ec306
AyAyEm <andre20114meira@gmail.com>
2021-03-09 17:10:48 +0000

    build: tsc build for 647514d487a3230c685edbaefcecdfa51b808b76

--- dist/examples/serialPortList.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const serialport_1 = tslib_1.__importDefault(require("serialport"));
-serialport_1.default.list().then(console.log);
-//# sourceMappingURL=serialPortList.js.map
