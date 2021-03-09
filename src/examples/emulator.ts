d45fc1969c3735b7447668050fe458e6f48ac65a
AyAyEm <andre20114meira@gmail.com>
2021-03-09 14:06:58 -0300

    Feat: added emulator example

---
+++ src/examples/emulator.ts
@@ -0,0 +1,17 @@
+import { PortInfo } from 'serialport';
+
+import { GenericEmulator, VidPidBalanceManager } from '..';
+
+const port: Partial<PortInfo> = { productId: 'testing', path: '/dev/ROBOT', vendorId: 'teste' };
+const manager = new VidPidBalanceManager([{ balanceModel: 'uranoPop', portId: port }]);
+const emulator = new GenericEmulator([port], manager);
+emulator.connect();
+
+manager.addListener('reading', (data) => {
+  console.log(data);
+  emulator.disconnectBalance();
+});
+
+manager.addListener('error', (error) => {
+  console.log(error);
+});
